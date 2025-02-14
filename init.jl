f622c72ddf9bf77d104ce1cf53c7a562baa52a77
Claudio Santiago <santiago10@llnl.gov>
2025-02-14 01:52:30 -0800

    removed comments

--- init.jl
+++ init.jl
@@ -1,30 +1,12 @@
 
-#import Pkg
-
-
-#Pkg.add("PyCall")
-#Pkg.build("PyCall")
-
 using PyCall
 
 repo_dir = dirname(Base.source_path())
 ENV["PYTHON"] = string(repo_dir,"/.pyenv/bin/python3")
 
-#println(ENV["PYTHON"])
-
-#exit()
-#Pkg.add("PyCall")
-#Pkg.build("PyCall")
-
-#using PyCall
-
 pushfirst!(pyimport("sys")."path", repo_dir)
 
-#println(repo_dir)
-#println(ENV["PYTHON"])
-#exit()
 tsslope_lib = pyimport("tsslope_lib")
-#exit()
 
 jl_lib = string(repo_dir,"/tsslope_lib_jl")
 

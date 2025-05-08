8f70881521ad9c0f02a2a751953705d021f5d86a
Chiang <chiang7@llnl.gov>
2025-05-08 13:25:06 -0700

    create branch for win machine

--- tsslope-pump/driver/driver.jl
+++ tsslope-pump/driver/driver.jl
@@ -3,7 +3,8 @@ include("load_config.jl")
 
 using Pkg;
 Pkg.activate((path_to_exajugo))
-push!(LOAD_PATH, string(path_to_exajugo, "/modules"))
+#push!(LOAD_PATH, string(path_to_exajugo, "/modules"))
+push!(LOAD_PATH, string("C:\\Users\\chiang7\\Project\\2025\\scidac\\exajugo", "\\modules"))
 
 include("exajugo_call.jl") 
 
@@ -11,10 +12,13 @@ using PyCall
 pushfirst!(pyimport("sys")."path", path_to_tsslope)
 tsslope_lib = pyimport("tsslope-pump-py")
 
-jl_lib = string(path_to_tsslope,"/tsslope-pump-jl")
-include(string(jl_lib,"/tsi_constraints.jl"))
+jl_lib = string(path_to_tsslope,"/tsslope-pump-jl") # linux
+include(string(jl_lib,"/tsi_constraints.jl")) # linux
 
-GPmodel, data, TSI = tsslope_lib.load_model(model_path, data_record)
+jl_lib = string(path_to_tsslope,"\\tsslope-pump-jl") # windowes
+include(string(jl_lib,"\\tsi_constraints.jl")) # windowes
+
+GPmodel, data, TSI = tsslope_lib.load_model(model_path, data_record);
 
 TSACOPF(case_path, case_sol_path, pf_limit_file, GPmodel);
 
